0d2b564942e998057f1a43f509dac3aef0505eee
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-10-24 01:39:12 -0400

    update

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'sIC9F11YNgdxjuV8mVp2UYekBV',
+  consumer_secret: 	'dfZdO3nGji51uLShGcBXqfC9yjrAGmEuouZcA65UleDFFnO7FE',
+  access_token_key: '922534575536033795-HuMbQAeQDf8z7gFE02AepEFjMpHbINR',
+  access_token_secret: 'aC1Y50GzMVBIjLKNYVdzHm5EX2NHozcH1kV2shnUmpUMA',
+}
+
+module.exports = {
+twitterKeys: twitterKeys
+}
---
+++ liri.js
@@ -0,0 +1,4 @@
+var keyList = require("./keys.js")
+
+console.log(twitterKeys.consumer_key);
+console.log(twitterKeys.access_token_key);
