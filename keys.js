f04bf5c0a20bcc70f0b4c1206506cab49a1375ca
Aaron Silverstein <adsilvercomp@Aarons-MacBook-Pro.local>
2017-10-24 20:11:21 -0400

    twitter api

--- keys.js
+++ keys.js
@@ -1,12 +1,12 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: 'sIC9F11YNgdxjuV8mVp2UYekBV',
-  consumer_secret: 	'dfZdO3nGji51uLShGcBXqfC9yjrAGmEuouZcA65UleDFFnO7FE',
+  consumer_key: 'rnBJQtKAMbQOZAm21QmbU5FOv',
+  consumer_secret: 	'PF7ryQYwd4oZ9b1VwWnlccmSk1dktuEWF3Y1YejxUzLFX0WNXk',
   access_token_key: '922534575536033795-HuMbQAeQDf8z7gFE02AepEFjMpHbINR',
   access_token_secret: 'aC1Y50GzMVBIjLKNYVdzHm5EX2NHozcH1kV2shnUmpUMA',
 }
 
 module.exports = {
-twitterKeys: twitterKeys
+twitterKeys:twitterKeys
 }
--- liri.js
+++ liri.js
@@ -1,4 +1,75 @@
 var keyList = require("./keys.js")
+var Twitter = require('twitter');
+var command = process.argv[2];
+var tweet
+var tweetTime
+var tweetArray = [];
 
-console.log(twitterKeys.consumer_key);
-console.log(twitterKeys.access_token_key);
+var client = new Twitter({
+ consumer_key: keyList.twitterKeys.consumer_key,
+ consumer_secret: keyList.twitterKeys.consumer_secret,
+ access_token_key: keyList.twitterKeys.access_token_key,
+ access_token_secret: keyList.twitterKeys.access_token_secret
+});
+
+var params = {
+  screen_name: '@silversteinmah1',
+  count: 20
+};
+
+
+switch (command) {
+  case "my-tweets":
+    console.log("hai")
+    myTweets();
+    break;
+
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+
+  case "movie-this":
+    movieThis();
+    break;
+
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+
+    //look up switch case default block- you have to add it incase none of the cases work.
+}
+
+
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+ if (!error) {
+
+  for(var x=0;x<20;x++){
+     tweet= tweets[x].text;
+     tweetTime=tweets[x].created_at;
+     tweetArray.push({
+      tweet:tweet,
+      tweetTime:tweetTime
+     })
+     
+  }
+ console.log('AFTER FOR LOOP');
+  console.log(tweetArray);
+  //console.log(tweets);
+  //console.log(response);
+  // console.log(error);
+ }
+ 
+ 
+ console.log(JSON.stringify(error));
+});
+
+function myTweets(){
+  console.log("hello");
+  tweetArray;
+}
+
+
+
+
+
